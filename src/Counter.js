8ec39e751ca7f5c56c2003d272174915dd1192f3
mokunshao <37831655+mokunshao@users.noreply.github.com>
2019-01-15 15:07:58 +0800

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import store from "./index";
+import { store } from "./index";
 
 class Counter extends React.Component {
   render() {
